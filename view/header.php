9eb75fb4c1f428cb957079f24ea308dd2b0266bc
Carlos <carlos.serra.ifp@gmail.com>
2026-04-23 09:49:36 +0200

    Añadido archivo header.php y header.css, y creado index.php para la estructura de la página principal.

---
+++ view/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="header.css" />
+</head>
+
+<body>
+
+    <header id="cabecera">
+        <div class="logo">
+            <a href="index.html">
+                <img src="assets/logo_cabecera_nombre_logo.png" id="logo_completo" alt="Logo_completo">
+                <img src="assets/logo en blanco.png" id="logo_solo" alt="Logo_solo"></a>
+        </div>
+        <div class="opciones">
+            <form class="buscador">
+                <input class="barra_buscador" type="search" placeholder="Buscar...">
+                <button type="submit" class="btn-buscar">
+                    <img src="assets/icono_lupa.png" alt="Buscar">
+                </button>
+            </form>
+
+            <div class="usuario">
+                <a href="formularios/formulario_crear_usuario.html" class="btn-registro">
+                    Registro
+                </a>
+
+                <a href="formularios/formulario_inicio_sesion_usuario.php" class="btn-login">
+                    Iniciar sesión
+                </a>
+                <!-- Botón único para móvil -->
+                <a href="formularios/formulario_inicio_sesion_usuario.html" class="btn-usuario">
+                </a>
+            </div>
+        </div>
+    </header>
+
+
+</body>
+
+</html>
--- view/index.php
+++ view/index.php
@@ -1,3 +1,4 @@
+<?php require_once ('header.php');?>
 <!DOCTYPE html>
 <html lang="es"> <!-- "es" si la web está en español -->
 
@@ -11,35 +12,6 @@
 <body>
     <div id="contenedor">
         <!-- CABECERA SUPERIOR -->
-        <header id="cabecera">
-            <div class="logo">
-                <a href="index.html">
-                    <img src="assets/logo_cabecera_nombre_logo.png" id="logo_completo" alt="Logo_completo">
-                    <img src="assets/logo en blanco.png" id="logo_solo" alt="Logo_solo"></a>
-            </div>
-            <div class="opciones">
-                <form class="buscador">
-                    <input class="barra_buscador" type="search" placeholder="Buscar...">
-                    <button type="submit" class="btn-buscar">
-                        <img src="assets/icono_lupa.png" alt="Buscar">
-                    </button>
-                </form>
-
-                <div class="usuario">
-                    <a href="formularios/formulario_crear_usuario.html" class="btn-registro">
-                        Registro
-                    </a>
-
-                    <a href="formularios/formulario_inicio_sesion_usuario.php" class="btn-login">
-                        Iniciar sesión
-                    </a>
-                    <!-- Botón único para móvil -->
-                    <a href="formularios/formulario_inicio_sesion_usuario.html" class="btn-usuario">
-                    </a>
-                </div>
-            </div>
-        </header>
-
 
         <main id="contenido">
             <section id="portada">
